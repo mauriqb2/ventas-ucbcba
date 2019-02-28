d05e00c9bd074e9af76e6a7a26574bee5234d09c
mauricio <mauriqb2@gmail.com>
2019-02-27 23:25:54 -0400

    test venta

---
+++ Venta.js
@@ -0,0 +1,10 @@
+let valor_total;
+class Venta{
+    constructor(){
+        valor_total = 0;
+    }
+    total(){
+        return valor_total;
+    }
+}
+module.exports = Venta;
--- test/venta_test.js
+++ test/venta_test.js
@@ -1,6 +1,11 @@
 var expect = require('chai').expect;
 var should = require('chai').should();
 
+import Venta from '../Venta.js'
+
 describe('Venta', function(){
-    
+    it('Total de venta sin producto deberia ser 0',function(){
+        let venta = new Venta();
+        expect(venta.total()).equal(0);
+    });
 });
